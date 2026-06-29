ca18188b508e8f6fbc6ff3b69ba9bcc071a50f22
Measrithy_Nazaby <nazabymeas7@gmail.com>
2026-06-29 18:27:52 +0700

    user

--- user_service/app/models/user.py
+++ user_service/app/models/user.py
@@ -1,4 +1,5 @@
-from sqlalchemy import String
+from datetime import datetime
+from sqlalchemy import String, Boolean, DateTime
 from sqlalchemy.orm import Mapped, mapped_column
 
 from app.core.database import Base
@@ -11,4 +12,7 @@ class User(Base):
     username: Mapped[str] = mapped_column(String(255), nullable=False)
     email: Mapped[str] = mapped_column(String(255), nullable=False)
     password: Mapped[str] = mapped_column(String(255), nullable=False)
-    is_active: Mapped[bool] = mapped_column()
+    is_active: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now())
+    updated_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now(), onupdate=datetime.now())
+    deleted_at: Mapped[datetime | None] = mapped_column(DateTime, nullable=True)
